f61d3a72a7b51022d34c831ca9bd44d3b44cff1f
Marco Quiña <marcoq558@gmail.com>
2023-12-19 15:53:30 -0500

    Adding/Change filters

--- backend/django_defensoria_universitaria/solicitudes_app/api/urls.py
+++ backend/django_defensoria_universitaria/solicitudes_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from solicitudes_app.api.views import ListarSolicitudAV, DetalleSolicitudAV, ListarArchivoAV, DetalleArchivoAV
+from solicitudes_app.api.views import ListarSolicitudAV, DetalleSolicitudAV, ListarArchivoAV, DetalleArchivoAV, DetalleSolicitudExpedienteAV, ListarTipoSolicitudAV, DetalleTipoSolicitudAV
 
 urlpatterns = [
     #Solicitud
@@ -9,4 +9,11 @@ urlpatterns = [
     #Archivo
     path('archivos/', ListarArchivoAV.as_view(), name='listar-archivos'),
     path('archivos/<int:pk>', DetalleArchivoAV.as_view(), name='detalle-archivo'),
+    
+    #Tipo solicitud
+    path('tipos/', ListarTipoSolicitudAV.as_view(), name='listar-tipo-solicitud'),
+    path('tipos/<int:pk>', DetalleTipoSolicitudAV.as_view(), name='detalle-tipo-solicitud'),
+    
+    #Solicitud por numero de expediente
+    path('expediente/', DetalleSolicitudExpedienteAV.as_view(), name='listar-solicitudes-numero-expediente'),
 ]
